d4443c20167b204120efa6ab156de84d25d0bf5a
haritsahm <haritsahm@gmail.com>
2025-04-04 10:21:39 +0700

    Add new init to walk_engines

---
+++ webots/controllers/omnidirectional_walk/walk_engines/__init__.py
@@ -0,0 +1,9 @@
+from .foot_step_planner import FootStepPlanner
+from .preview_control import PreviewControl
+from .open_zmp_walk import OpenZMPWalk
+
+__all__ = [
+    'OpenZMPWalk',
+    'FootStepPlanner',
+    'PreviewControl'
+]
